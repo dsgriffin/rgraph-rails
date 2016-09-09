d94434b30b7ef368256e447f91fb47d32a2f9324
Daniel Griffin <danielseangriffin@gmail.com>
2016-09-09 13:47:44 +0100

    Added minified RGraph components, removed unused folders

--- spec/rgraph-rails_spec.rb
+++ spec/rgraph-rails_spec.rb
@@ -4,8 +4,4 @@ describe RgraphRails do
   it 'has a version number' do
     expect(RgraphRails::VERSION).not_to be nil
   end
-
-  it 'does something else' do
-    expect(true).to eq(true)
-  end
 end
